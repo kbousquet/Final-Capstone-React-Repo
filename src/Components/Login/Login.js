17ea4011f64e3a0b6430d76efc6f665dc25f4a97
kbousquet <kbousquet95@gmail.com>
2024-02-27 21:57:01 -0500

    add log in functionality

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,26 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 import './Login.css';
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [emailError, setEmailError] = useState('');
+   
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (sessionStorage.getItem("auth-token")) {
+        navigate("/")
+        }
+    }, []);
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-        setEmailError('');
-    };
-
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
-
-    const handleSubmit = (e) => {
+    const login = async (e) => {
         e.preventDefault();
-        if (!/\S+@\S+\.\S+/.test(email)) {
-            setEmailError('Please enter a valid email address.');
-            return;
-          }
+        const res = await fetch(`${API_URL}/api/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                email:email,
+                password: password,
+            }),
+        });
+        const json = await res.json();
+        if (json.authtoken) {
+            sessionStorage.setItem('auth-token', json.authtoken);
+        
+            sessionStorage.setItem('email', email);
+            navigate('/');
+            window.location.reload()
+        } else {
+            if (json.errors) {
+                for (const error of json.errors) {
+                    if (error.param === 'email') setEmailError(error.msg);
+                }
+            } else {
+                alert(json.error);
+            }
+        }
     };
    
     return (
@@ -32,17 +54,17 @@ const Login = () => {
                 <div className="login-text1" style={{textAlign: "left"}}>
                     New member? <span><a href="../Signup"> Sign Up</a></span>
                 </div>
-                <form onSubmit={handleSubmit} noValidate>
+                <form onSubmit={login} noValidate>
                     <div className="form-group">
                         <label for="email">Email</label>
-                        <input type="email" name="email" id="email" value={email} onChange={handleEmailChange} required className={`form-control ${emailError ? 'is-invalid' : ''}`} placeholder="Enter your email" aria-describedby="helpId" />
+                        <input type="email" name="email" id="email" value={email} onChange={(e) => setEmail(e.target.value)} required className={`form-control ${emailError ? 'is-invalid' : ''}`} placeholder="Enter your email" aria-describedby="helpId" />
                         <div className="error-container">
-                            <div className="invalid-feedback">{emailError}</div>
+                            <div className="err">{emailError}</div>
                         </div>
                     </div>
                     <div className="form-group">
                         <label for="password">Password</label>
-                        <input type="password" name="password" id="password" value={password} onChange={handlePasswordChange} required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
+                        <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required className="form-control" placeholder="Enter your password" aria-describedby="helpId" />
                     </div>
                     <div className="btn-group">
                         <button type="submit" className="btn btn-submit mb-2 mr-1 waves-effect waves-light">Submit</button>
