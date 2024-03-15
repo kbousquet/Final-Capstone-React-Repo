82ca7a862e16a4c9c46513af58267d8714399e84
kbousquet <kbousquet95@gmail.com>
2024-03-14 22:17:28 -0400

    move Notification component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from './Components/Navbar/Navbar';
 import LandingPage from './Components/Landing_Page/LandingPage';
 import Signup from './Components/Sign_Up/Signup';
 import Login from './Components/Login/Login';
@@ -13,19 +12,19 @@ function App() {
     return (
         <>
             <BrowserRouter>
-                <Navbar/>
                 <div id="main">
-                    <Routes>
-                        <Route path="/" element={<LandingPage/>}/>
-                        <Route path="/home" element={<LandingPage/>}/>
-                        <Route path="/signup" element={<Signup/>}/>
-                        <Route path="/login" element={<Login/>}/>
-                        <Route path="/services/instant-consultation" element={<InstantConsultation />}/>
-                        <Route path="/services/appointments" element={<BookingConsultation />}/>
-                        <Route path="/services" element={<Services />}/>
-                    </Routes>
-                    <Notification />
-                </div>       
+                    <Notification>
+                        <Routes>
+                            <Route path="/" element={<LandingPage/>}/>
+                            <Route path="/home" element={<LandingPage/>}/>
+                            <Route path="/signup" element={<Signup/>}/>
+                            <Route path="/login" element={<Login/>}/>
+                            <Route path="/services/instant-consultation" element={<InstantConsultation />}/>
+                            <Route path="/services/appointments" element={<BookingConsultation />}/>
+                            <Route path="/services" element={<Services />}/>
+                        </Routes>   
+                    </Notification>  
+                </div>   
             </BrowserRouter>
         </>
     );
--- src/Components/Notification/Notification.js
+++ src/Components/Notification/Notification.js
@@ -2,16 +2,25 @@ import React, { useEffect, useState } from 'react';
 import expandChevron from '../../Images/expand-chevron.svg';
 import collapseChevron from '../../Images/collapse-chevron.svg';
 import './Notification.css';
-const Notification = () => {
+import Navbar from '../Navbar/Navbar';
+
+const Notification = ({children}) => {
     const [expanded, setExpanded] = useState(false);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [appointmentData, setAppointmentData] = useState(null);
     
     useEffect(() => {
-      const str = localStorage.getItem("appointmentData");
-      const storedAppointmentData = JSON.parse(str);
-      if (storedAppointmentData) {
-        setAppointmentData(storedAppointmentData);
-      }
+        const storedUsername = sessionStorage.getItem('email');
+        const str = localStorage.getItem("appointmentData");
+        const storedAppointmentData = JSON.parse(str);
+
+        if (storedAppointmentData) {
+            setAppointmentData(storedAppointmentData);
+        }
+
+        if (storedUsername) {
+            setIsLoggedIn(true);
+        }
     }, []);
 
 const toggleExpanded = () => {
@@ -20,13 +29,14 @@ const toggleExpanded = () => {
 
   return (
     <>
-        {appointmentData && (
+        <Navbar />
+        {children}
+        {appointmentData && isLoggedIn && (
             <div className={(expanded ? 'notification-container expanded' : 'notification-container')}>
                 <button className="expand-btn" onClick={toggleExpanded}>
                 {expanded ? <img src={collapseChevron} alt="Collapse" width="20px" height="20px" /> : <img src={expandChevron} alt="Expand" width="20px" height="20px" />}
                 <h3>Appointment Details</h3>
                 </button>
-                {/* {expanded &&  */}
                     <>
                         <p>
                             <strong>Doctor:</strong> {appointmentData?.doctorName}
@@ -38,7 +48,6 @@ const toggleExpanded = () => {
                             <strong>Date & Time:</strong> {appointmentData?.dateTime}
                         </p>
                     </>
-                {/* } */}
             </div>
         )}
     </>
